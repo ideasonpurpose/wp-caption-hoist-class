b56e525912954648ed0474db0c883e979c89cd6d
Joe Maller <joe@ideasonpurpose.com>
2017-02-11 14:48:16 -0500

    first commit

---
+++ src/CaptionHoistClass.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ideasonpurpose;
+
+class CaptionHoistClass
+{
+    public function __construct()
+    {
+        add_filter('', [$this, 'mce_load']);
+    }
+
+    public function mce_load($plugins)
+    {
+      $plugins['caption-hoist-class'] = plugins_url('caption-hoise-class', __file__);
+      return $plugins;
+    }
+}
